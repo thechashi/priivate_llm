0b1aeca829b3c00d0ee5f32dcab3c2f2a1177f8e
chashi-mahiul-islam-bd <ci20l@my.fsu.edu>
2023-07-28 14:42:49 -0400

    core: libraries updated

---
+++ setup.sh
@@ -0,0 +1,5 @@
+pip install -U bitsandbytes
+pip install -U git+https://github.com/huggingface/transformers.git 
+pip install -U git+https://github.com/huggingface/peft.git
+pip install -U git+https://github.com/huggingface/accelerate.git
+pip install datasets
